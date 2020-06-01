d8ba6e1e30a2d5a6099873d20e5af1809cb21b42
jason-s-yu <jyu031103@gmail.com>
2020-06-01 02:42:09 -0700

    Update infile

--- database/infile.sql
+++ database/infile.sql
@@ -6,17 +6,17 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 DROP TABLE IF EXISTS "User" CASCADE;
 CREATE TABLE IF NOT EXISTS "public"."User" (
-  id        UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4 (),
-  username  VARCHAR(255),
-  email     VARCHAR(255) UNIQUE NOT NULL,
-  password  VARCHAR(255),
-  elo       INTEGER
+  "id"        UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4 (),
+  "username"  VARCHAR(255),
+  "email"     VARCHAR(255) UNIQUE NOT NULL,
+  "password"  VARCHAR(255),
+  "elo"       INTEGER
 );
 
 DROP TABLE IF EXISTS "Game" CASCADE;
 CREATE TABLE IF NOT EXISTS "public"."Game" (
-  id            UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4 (),
-  duration      INTEGER, /* in seconds */
+  "id"            UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4 (),
+  "duration"      INTEGER, /* in seconds */
   "playerCount" INTEGER
 );
 
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS "public"."Game" (
  */
 DROP TABLE IF EXISTS "Match" CASCADE;
 CREATE TABLE IF NOT EXISTS "public"."Match" (
-  id          SERIAL PRIMARY KEY NOT NULL,
+  "id"          SERIAL PRIMARY KEY NOT NULL,
   "userId"    UUID NOT NULL,
   "gameId"    UUID NOT NULL,
   FOREIGN KEY ("userId") REFERENCES "public"."User"(id),
