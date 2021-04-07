52cb170a283cd280ac0fe1466b976e6fcec1d076
jason-s-yu <jyu031103@gmail.com>
2021-04-07 09:29:43 -0700

    Fix build process

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,52 +2,57 @@ import gulp from 'gulp';
 import typescript from 'gulp-typescript';
 import merge from 'merge-stream';
 import del from 'del';
-import { createInterface } from 'readline';
+import readlineSync from 'readline-sync';
 import fs from 'fs';
 
 const env = {
-  test: '',
-  blah: ''
-};
+  DEVMODE: false,
+  DATABASE_URL: 'DATABASE_URL="postgres://user:pass@host:port/db'
+}
 
 const dir = {
   src: 'src',
-  build: 'build'
+  dist: 'dist'
 };
 
-gulp.task('init', () => {
-  let result;
-  if (!fs.existsSync('prisma/.env')) {
-    // interface to read from command line
-    const readline = createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-
-    for (const property in env) {
-      readline.question(`${property.toString()}${env[property] ? ` (${env[property]})` : ''}: `, ans => {
-        const write = `${env[property]}=${ans}`;
-        fs.appendFile('prisma/.env', write, err => console.error(err));
-        readline.close();
-      });
-    }
-
-    result = gulp.src('prisma/.env').pipe(gulp.dest('build'));
+const fileExists = async (file) => {
+  try {
+    return fs.promises.access(file, fs.constants.F_OK)
+    .then(() => true);
+  } catch (err) {
+    return false;
   }
-  result = gulp.src('prisma/.env')
-    .pipe(gulp.dest('build'));
-  return result;
+}
+
+gulp.task('init', async () => {
+  fs.access('.env', (err) => {
+    if (err) {
+      console.log('.env does not exist. Please answer the following prompts: (default listed in parens)');
+      for (const property in env) {
+        const ans = readlineSync.question(`${property.toString()}${env[property] != undefined ? ` (${env[property]})` : ''}: `);
+        if (!ans) {
+          ans = env[property];
+        }
+        const write = `${property}=${ans}\n`;
+        fs.appendFile('.env', write, err => console.error(err));
+      }
+    }
+    
+    const result = gulp.src('.env')
+      .pipe(gulp.dest(dir.dist));
+    return result;
+  });
 });
 
-gulp.task('clean', () => del([ dir.build ]))
+gulp.task('clean', () => del([ dir.dist ]))
 
 gulp.task('server', () => {
   let entry = gulp.src(`${dir.src}/index.ts`)
     .pipe(typescript(require('./tsconfig.json').compilerOptions))
-	.pipe(gulp.dest(`${dir.build}/${dir.src}`));
+	.pipe(gulp.dest(`${dir.dist}/${dir.src}`));
   let everything_else = gulp.src('./*(!(node_modules|tests))/**/*.ts')
     .pipe(typescript(require('./tsconfig.json').compilerOptions))
-    .pipe(gulp.dest(`${dir.build}`));
+    .pipe(gulp.dest(`${dir.dist}`));
   return merge(entry, everything_else);
 });
 
